5529165309f0c363e91ab4f215b2ceba376a36c0
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 10:51:18 +0200

    Create PaypalWebCheckoutManager.php

---
+++ Services/PaypalWebCheckoutManager.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle\Services;
+
+use PaymentSuite\PaymentCoreBundle\Services\Interfaces\PaymentBridgeInterface;
+use PaymentSuite\PaymentCoreBundle\Services\PaymentEventDispatcher;
+use PaymentSuite\PaypalWebCheckoutBundle\PaypalWebCheckoutMethod;
+
+/**
+ * Paypal Web Checkout manager
+ */
+class PaypalWebCheckoutManager
+{
+    /**
+     * @var PaymentEventDispatcher
+     *
+     * Payment event dispatcher
+     */
+    protected $paymentEventDispatcher;
+
+    /**
+     * @var PaymentBridgeInterface
+     *
+     * Payment bridge interface
+     */
+    protected $paymentBridge;
+
+    /**
+     * @var PaypalWebCheckoutTransactionWrapper $paypalWrapper
+     *
+     * Paypal Web Checkout wrapper
+     */
+    protected $paypalWrapper;
+
+    /**
+     * @var Array config
+     *
+     * Paypal Web Checkout configuration
+     */
+    protected $config;
+
+    /**
+     * Construct method for paypal manager
+     *
+     * @param PaymentEventDispatcher $paymentEventDispatcher Event dispatcher
+     * @param PaymentBridgeInterface $paymentBridge          Payment Bridge
+     * @param PaypalWebCheckoutTransactionWrapper Paypal Wrapper
+     */
+    public function __construct(PaymentEventDispatcher $paymentEventDispatcher, PaymentBridgeInterface $paymentBridge, PaypalWebCheckoutTransactionWrapper $paypalWrapper)
+    {
+        $this->paymentEventDispatcher = $paymentEventDispatcher;
+        $this->paymentBridge = $paymentBridge;
+        $this->paypalWrapper = $paypalWrapper;
+    }
+
+    /**
+     * See also PaypalWebCheckout Api Integration : https://developer.paypal.com/docs/integration/web/web-checkout/
+     */
+    public function preparePayment(PaypalExpressCheckoutMethod $paypalMethod)
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function processPayment(PaypalExpressCheckoutMethod $paymentMethod)
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function getPaymentStatus(PaypalExpressCheckoutTransactionWrapper $paypalWrapper)
+    {
+        
+    }
+}
