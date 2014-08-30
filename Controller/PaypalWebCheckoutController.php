98675fc23376420ba81737993cfd76d22a08f9d9
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 10:42:12 +0200

    Create PaypalWebCheckoutController.php

---
+++ Controller/PaypalWebCheckoutController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+ 
+namespace PaymentSuite\PaypalWebCheckoutBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use PaymentSuite\PaypalWebCheckoutBundle\Exception\PaymentException;
+
+/**
+ * PaypalWebCheckoutController
+ */
+class PaypalWebCheckoutController extends Controller
+{
+    /**
+     * Payment execution
+     *
+     * @param Request $request Request element
+     *
+     * @return Response
+     *
+     * @Method("GET")
+     */
+    public function executeAction(Request $request)
+    {
+        $formView = $this->get('paypal.manager')->processPayment();
+
+        return $this->render('PaypalWebCheckoutBundle:Paypal:process.html.twig',array(
+            'paypal_form' => $formView,
+        ));
+    }
+}
