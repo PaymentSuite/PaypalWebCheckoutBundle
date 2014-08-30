120b92a78770e9f1330981f12874bafeeedecdc4
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 11:06:37 +0200

    Create PaypalWebCheckoutRoutesLoader.php

---
+++ Router/PaypalWebCheckoutRoutesLoader.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle\Router;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Paypal router
+ */
+class PaypalWebCheckoutRoutesLoader implements LoaderInterface
+{
+    /**
+     * @var string
+     *
+     * Execution route name
+     */
+    private $controllerRouteName;
+
+    /**
+     * @var string
+     *
+     * Execution controller route
+     */
+    private $controllerRoute;
+
+    /**
+     * @var boolean
+     *
+     * Route is loaded
+     */
+    private $loaded = false;
+
+    /**
+     * Construct method
+     *
+     * @param string $controllerRouteName Controller route name
+     * @param string $controllerRoute     Controller route
+     */
+    public function __construct($controllerRouteName, $controllerRoute)
+    {
+        $this->controllerRouteName = $controllerRouteName;
+        $this->controllerRoute = $controllerRoute;
+    }
+
+    /**
+     * Loads a resource.
+     *
+     * @param mixed  $resource The resource
+     * @param string $type     The resource type
+     *
+     * @return RouteCollection
+     *
+     * @throws RuntimeException Loader is added twice
+     */
+    public function load($resource, $type = null)
+    {
+        if ($this->loaded) {
+
+            throw new \RuntimeException('Do not add this loader twice');
+        }
+
+        $routes = new RouteCollection();
+        $routes->add($this->controllerRouteName, new Route($this->controllerRoute, array(
+            '_controller' => 'PaypalWebCheckoutBundle:PaypalWebCheckout:execute',
+        )));
+
+        $this->loaded = true;
+
+        return $routes;
+    }
+
+    /**
+     * Returns true if this class supports the given resource.
+     *
+     * @param mixed  $resource A resource
+     * @param string $type     The resource type
+     *
+     * @return boolean true if this class supports the given resource, false otherwise
+     */
+    public function supports($resource, $type = null)
+    {
+        return 'paypal_web_checkout' === $type;
+    }
+}
