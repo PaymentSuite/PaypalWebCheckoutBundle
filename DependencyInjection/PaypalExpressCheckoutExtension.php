41ca02d993cb84648adecb272871920d715b1353
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 10:47:28 +0200

    Create PaypalExpressCheckoutExtension.php

---
+++ DependencyInjection/PaypalExpressCheckoutExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class PaypalExpressCheckoutExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('paypal_web_checkout.business', $config['business']);
+        $container->setParameter('paypal_web_checkout.debug', $config['debug']);
+        $container->setParameter('paypal_web_checkout.controller.route', $config['controller_route']);
+
+        $container->setParameter('paypal_web_checkout.success.route', $config['payment_success']['route']);
+        $container->setParameter('paypal_web_checkout.success.order.append', $config['payment_success']['order_append']);
+        $container->setParameter('paypal_web_checkout.success.order.field', $config['payment_success']['order_append_field']);
+
+        $container->setParameter('paypal_web_checkout.fail.route', $config['payment_fail']['route']);
+        $container->setParameter('paypal_web_checkout.fail.order.append', $config['payment_fail']['order_append']);
+        $container->setParameter('paypal_web_checkout.fail.order.field', $config['payment_fail']['order_append_field']);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
+        $loader->load('services.yml');
+    }
+}
