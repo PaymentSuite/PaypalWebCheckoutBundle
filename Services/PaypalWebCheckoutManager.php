e7af6cae5ea93eaeb59a56c4c24560392189eda9
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 12:37:48 +0200

    Update PaypalWebCheckoutManager.php

--- Services/PaypalWebCheckoutManager.php
+++ Services/PaypalWebCheckoutManager.php
@@ -12,9 +12,12 @@
 
 namespace PaymentSuite\PaypalWebCheckoutBundle\Services;
 
+use PaymentSuite\PaymentCoreBundle\Exception\PaymentException;
+use PaymentSuite\PaymentCoreBundle\Exception\PaymentOrderNotFoundException;
 use PaymentSuite\PaymentCoreBundle\Services\Interfaces\PaymentBridgeInterface;
 use PaymentSuite\PaymentCoreBundle\Services\PaymentEventDispatcher;
 use PaymentSuite\PaypalWebCheckoutBundle\PaypalWebCheckoutMethod;
+use PaymentSuite\PaypalWebCheckoutBundle\Form\Type\PaypalFormTypeWrapper;
 use PaymentSuite\PaypalWebCheckoutBundle\Services\Wrapper\PaypalWebCheckoutTransactionWrapper;
 
 /**
@@ -73,6 +76,41 @@ class PaypalWebCheckoutManager
     public function processPayment()
     {
         $paypalMethod = new PaypalWebCheckoutMethod();
+        
+        /**
+         * At this point, order must be created given a cart, and placed in PaymentBridge
+         *
+         * So, $this->paymentBridge->getOrder() must return an object
+         */
+        $this
+            ->paymentEventDispatcher
+            ->notifyPaymentOrderLoad(
+                $this->paymentBridge,
+                $paypalMethod
+            );
+
+        /**
+         * Order Not found Exception must be thrown just here
+         */
+        if (!$this->paymentBridge->getOrder()) {
+            throw new PaymentOrderNotFoundException;
+        }
+
+        /**
+         * Order exists right here
+         */
+        $this
+            ->paymentEventDispatcher
+            ->notifyPaymentOrderCreated(
+                $this->paymentBridge,
+                $paypalMethod
+            );
+
+        $formView = $this
+            ->paypalFormTypeWrapper
+            ->buildForm();
+
+        return $formView;
     }
 
     /**
