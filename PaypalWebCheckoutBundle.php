aa369c33b2a80daa554fe3c5650f88c578e82593
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 10:10:30 +0200

    Create PaypalWebCheckoutBundle.php

---
+++ PaypalWebCheckoutBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Paypal web payment bundle
+ */
+class PaypalWebCheckoutBundle extends Bundle
+{
+
+}
