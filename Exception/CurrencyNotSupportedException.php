b85973c5a4189fb7be1b48eab97e220006770182
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-08-30 17:39:13 +0200

    Create CurrencyNotSupportedException.php

---
+++ Exception/CurrencyNotSupportedException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle\Exception;
+
+use PaymentSuite\PaymentCoreBundle\Exception\PaymentException;
+
+/**
+ * CurrencyNotSupportedException
+ */
+class CurrencyNotSupportedException extends PaymentException
+{
+
+}
