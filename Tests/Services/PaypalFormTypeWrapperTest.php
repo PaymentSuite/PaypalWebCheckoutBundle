855b459e7a0c6a2cb789bc00d5fe82b6b1180222
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-09-01 21:43:55 +0200

    Add form creation test

---
+++ Tests/Services/PaypalFormTypeWrapperTest.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\PaypalWebCheckoutBundle\Tests\Services;
+
+use Symfony\Component\Form\Test\TypeTestCase;
+
+use PaymentSuite\PaypalWebCheckoutBundle\Services\Wrapper\PaypalFormTypeWrapper;
+
+/**
+ * Class PaypalFormTypeWrapperTest
+ */
+class PaypalFormTypeWrapperTest extends TypeTestCase
+{
+    /**
+     * @var string
+     *
+     * Business code
+     */
+    const business = 'arkaitz.garro-facilitator@gmail.com';
+
+    /**
+     * @var string
+     */
+    const url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+
+    /**
+     * @var string
+     */
+    const returnRouteName = 'payment_success';
+
+    /**
+     * @var string
+     */
+    const cancelRouteName = 'payment_failed';
+
+    /**
+     * @var string
+     */
+    const notifyRouteName = 'payment_process';
+
+    /**
+     * @var PaymentBridge
+     *
+     * Payment bridge object
+     */
+    private $paymentBridge;
+
+    /**
+     * @var PaymentEventDispatcher
+     *
+     * Payment event dispatcher object
+     */
+    private $paymentEventDispatcher;
+
+    /**
+     * @var PaypalMethod
+     *
+     * Paypal method object
+     */
+    private $paypalMethod;
+
+    /**
+     * @var PaypalFormTypeWrapper
+     *
+     * Paypal form type manager object
+     */
+    private $paypalFormTypeWrapper;
+
+    /**
+     * Setup method
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentBridge = $this
+            ->getMockBuilder('Mmoreram\PaymentBridgeBundle\Services\PaymentBridge')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->paymentEventDispatcher = $this
+            ->getMockBuilder('PaymentSuite\PaymentCoreBundle\Services\PaymentEventDispatcher')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->paypalMethod = $this
+            ->getMockBuilder('PaymentSuite\PaypalWebCheckoutBundle\PaypalMethod')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $router = $this->getMockBuilder('Symfony\Bundle\Frameworkbundle\Routing\Router')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->paypalFormTypeWrapper = new PaypalFormTypeWrapper($this->factory,
+            $this->paymentBridge,
+            $router,
+            $this::business,
+            $this::url,
+            $this::returnRouteName,
+            $this::cancelRouteName,
+            $this::notifyRouteName,
+            true,
+            'www.sandbox');
+    }
+
+    /**
+     * Test form creation
+     */
+    public function testFormCreation()
+    {
+        $amount = 10;
+
+        $formData = array(
+            'amount' => $amount * 100
+        );
+
+        $formView = $this->paypalFormTypeWrapper->buildForm();
+
+        $children = $formView->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+            $message = $formData[$key].'/'.$children[$key]->vars['value'];
+            $this->assertEquals($formData[$key], $children[$key]->vars['value'], $message);
+        }
+    }
+}
