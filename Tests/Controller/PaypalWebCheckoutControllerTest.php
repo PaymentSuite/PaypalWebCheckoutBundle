6e8baedd3282a642b0c0b034a43e98a40535fb57
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-09-01 19:19:00 +0200

    Create test for main controller

---
+++ Tests/Controller/PaypalWebCheckoutControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PaypalWebCheckout for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Arkaitz Garro <hola@arkaitzgarro.com>
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace PaymentSuite\RedsysBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class PaypalWebCheckoutControllerTest
+ */
+class PaypalWebCheckoutControllerTest extends WebTestCase
+{
+    /**
+     * Test execute controller
+     */
+    public function testExecute()
+    {
+    }
+}
